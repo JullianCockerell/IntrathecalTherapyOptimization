8c715465f5ef26623718bbbbc4bf6b95a1dd601f
John Smithy <johnsmith@johns-imac.local>
2018-10-28 16:29:22 -0700

    Barely started nixing code

--- IntrathecalTherapyOptimization/View_ConstantFlow.swift
+++ IntrathecalTherapyOptimization/View_ConstantFlow.swift
@@ -610,24 +610,6 @@ class View_ConstantFlow: UIViewController {
         initializeUI()
     }
     
-    @objc func keyboardWillShow(notification: NSNotification)
-    {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0{
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-        }
-    }
-    
-    @objc func keyboardWillHide(notification: NSNotification)
-    {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y != 0{
-                self.view.frame.origin.y += keyboardSize.height
-            }
-        }
-    }
-    
     
     override func didReceiveMemoryWarning()
     {
