864c1e276f483de6291357babe9934c84bd45d96
John Smithy <johnsmith@johns-imac.local>
2018-12-11 14:50:36 -0800

    Some Compare Flow functionality and debugging, rest is complete

--- IntrathecalTherapyOptimization/View_CompareFlow.swift
+++ IntrathecalTherapyOptimization/View_CompareFlow.swift
@@ -28,6 +28,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
     var minute2 = Float(0)
     var minute3 = Float(0)
     var minute4 = Float(0)
+    var unitLabel = "mg"
     
 
     //General
@@ -38,6 +39,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
     @IBOutlet weak var multirateView: UIView!
     @IBOutlet weak var periodicView: UIView!
     @IBOutlet weak var graphDisplay: UIImageView!
+    @IBOutlet weak var graphBorder: UIView!
     weak var constantShapeLayer: CAShapeLayer?
     weak var periodicShapeLayer: CAShapeLayer?
     weak var multiRateShapeLayer: CAShapeLayer?
@@ -704,6 +706,18 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
             }
             updateMultirate()
         }
+        else if(textField == masterConcentrationField)
+        {
+            activateInputs()
+            let pumpConText = masterConcentrationField.text!
+            let pumpConFloat = (pumpConText as NSString).floatValue
+            let dosePerClick = accumVol * pumpConFloat
+            let dosePerClickRounded = Double(dosePerClick * 100).rounded() / 100
+            dosePerClickField.text = "\(dosePerClickRounded)" + " " + unitLabel
+            updateConstant()
+            updatePeriodic()
+            updateMultirate()
+        }
         else
         {
             activateInputs()
@@ -725,18 +739,83 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
     {
         if(masterUnitSwitch.isOn)
         {
-            
+            mgMode = true
+            unitLabel = "mg"
+            masterConcentrationLabel.text = unitLabel
+            constantDoseInputLabel.text = unitLabel
+            periodicDoseInputLabel.text = unitLabel
+            multirateDoseLabel1.text = unitLabel
+            multirateDoseLabel2.text = unitLabel
+            multirateDoseLabel3.text = unitLabel
+            multirateDoseLabel4.text = unitLabel
+            constantDoseSlider.maximumValue = 5
+            masterConcentrationField.text = "10.0"
+            constantDoseSlider.value = 0.5
+            constantDoseInputField.text = "0.5"
+            periodicDoseSlider.value = 0.5
+            periodicDoseSlider.maximumValue = 5
+            periodicDoseInputField.text = "0.5"
+            multirateDoseSlider1.maximumValue = 5
+            multirateDoseSlider1.value = 0.5
+            multirateDoseField1.text = "0.5"
+            multirateDoseSlider2.maximumValue = 5
+            multirateDoseSlider2.value = 1.0
+            multirateDoseField2.text = "1.0"
+            multirateDoseSlider3.maximumValue = 5
+            multirateDoseSlider3.value = 1.5
+            multirateDoseField3.text = "1.5"
+            multirateDoseSlider4.maximumValue = 5
+            multirateDoseSlider4.value = 2.0
+            multirateDoseField4.text = "2.0"
+            updateConstant()
+            updatePeriodic()
+            updateMultirate()
         }
         else
         {
-            
+            mgMode = false
+            unitLabel = "mcg"
+            masterConcentrationLabel.text = unitLabel
+            constantDoseInputLabel.text = unitLabel
+            periodicDoseInputLabel.text = unitLabel
+            multirateDoseLabel1.text = unitLabel
+            multirateDoseLabel2.text = unitLabel
+            multirateDoseLabel3.text = unitLabel
+            multirateDoseLabel4.text = unitLabel
+            constantDoseSlider.maximumValue = 300
+            masterConcentrationField.text = "350.0"
+            constantDoseSlider.value = 10
+            constantDoseInputField.text = "10"
+            periodicDoseSlider.value = 20
+            periodicDoseSlider.maximumValue = 300
+            periodicDoseInputField.text = "20"
+            multirateDoseSlider1.maximumValue = 200
+            multirateDoseSlider1.value = 20
+            multirateDoseField1.text = "20"
+            multirateDoseSlider2.maximumValue = 200
+            multirateDoseSlider2.value = 30
+            multirateDoseField2.text = "30"
+            multirateDoseSlider3.maximumValue = 200
+            multirateDoseSlider3.value = 40
+            multirateDoseField3.text = "40"
+            multirateDoseSlider4.maximumValue = 200
+            multirateDoseSlider4.value = 50
+            multirateDoseField4.text = "50"
+            updateConstant()
+            updatePeriodic()
+            updateMultirate()
         }
+        let pumpConText = masterConcentrationField.text!
+        let pumpConFloat = (pumpConText as NSString).floatValue
+        let dosePerClick = accumVol * pumpConFloat
+        let dosePerClickRounded = Double(dosePerClick * 100).rounded() / 100
+        dosePerClickField.text = "\(dosePerClickRounded)" + " " + unitLabel
     }
     
     func updatePeriodic() -> Void
     {
         let totalDose = Double(periodicDoseSlider.value) * periodicBolusStepper.value
-        periodicTotalDoseField.text = "\(totalDose)"
+        periodicTotalDoseField.text = "\(totalDose)" + " " + unitLabel
         if(periodicShow)
         {
             genPeriodicGraph()
@@ -762,7 +841,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         {
             totalDose = multirateDoseSlider1.value + multirateDoseSlider2.value + multirateDoseSlider3.value + multirateDoseSlider4.value
         }
-        multiRateTotalDoseField.text = "\(totalDose)"
+        multiRateTotalDoseField.text = "\(totalDose)" + " " + unitLabel
         if(multirateShow)
         {
             genMultiRateGraph()
@@ -792,7 +871,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         {
             constantPumpRateField.text = "1 Valve Actuation every " + timeBetween + " minutes"
         }
-        constantTotalDoseField.text = "\(totalDose)"
+        constantTotalDoseField.text = "\(totalDose)" + " " + unitLabel
         if(constantShow)
         {
             genConstantGraph()
@@ -824,7 +903,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         //remove old shape layer if any is present
         self.constantShapeLayer?.removeFromSuperlayer()
         let graphX = Float(graphDisplay.frame.origin.x)
-        let graphY = Float(graphDisplay.frame.origin.y)
+        let graphY = Float(graphDisplay.frame.origin.y) - 3
         let graphWidth = Float(graphDisplay.bounds.width)
         let graphHeight = Float(graphDisplay.bounds.height)
         
@@ -836,7 +915,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         let pumpCon = masterConcentrationField.text!
         let pumpConFloat = (pumpCon as NSString).floatValue
         var bolNum = Int(((totalDose / pumpConFloat) / accumVol))
-        let bolHeight = ((accumVol * pumpConFloat) / yLabelMax) * graphHeight
+        let bolHeight = ((accumVol / Float(1)) * graphHeight) + 8
         let bolSpacing = graphWidth / Float(bolNum)
         bolNum += 1
         
@@ -917,12 +996,12 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         //create shape layer for that path
         let shapeLayer = CAShapeLayer()
         shapeLayer.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
-        shapeLayer.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        shapeLayer.strokeColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).cgColor
         shapeLayer.lineWidth = 3
         shapeLayer.path = path.cgPath
         shapeLayer.lineCap = kCALineCapRound
         shapeLayer.lineJoin = kCALineJoinRound
-        view.layer.addSublayer(shapeLayer)
+        scrollView.layer.addSublayer(shapeLayer)
         
         //save shape layer to viewcontroller
         self.constantShapeLayer = shapeLayer
@@ -937,7 +1016,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         let graphWidth = Float(graphDisplay.bounds.width)
         let graphHeight = Float(graphDisplay.bounds.height)
         let graphX2 = graphX
-        let graphY2 = graphY
+        let graphY2 = graphY - 3
         
         //create path for graph to draw
         let path = UIBezierPath()
@@ -1103,24 +1182,14 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
             }
         }
         
-        //create shape layer for that path
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
-        shapeLayer.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        shapeLayer.lineWidth = 3
-        shapeLayer.path = path.cgPath
-        shapeLayer.lineCap = kCALineCapRound
-        shapeLayer.lineJoin = kCALineJoinRound
-        //view.layer.addSublayer(shapeLayer)
-        
         let shapeLayer2 = CAShapeLayer()
         shapeLayer2.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
-        shapeLayer2.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        shapeLayer2.strokeColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1).cgColor
         shapeLayer2.lineWidth = 3
         shapeLayer2.path = path2.cgPath
         shapeLayer2.lineCap = kCALineCapRound
         shapeLayer2.lineJoin = kCALineJoinRound
-        view.layer.addSublayer(shapeLayer2)
+        scrollView.layer.addSublayer(shapeLayer2)
         
         //save shape layer to viewcontroller
         self.periodicShapeLayer = shapeLayer2
@@ -1140,9 +1209,11 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         //ok to use cartesian coordinates, will be shifted when assigning points to path
         var coordArray = [[Int]]()
         var c = 0
+        let pumpConText = masterConcentrationField.text!
+        let pumpConFloat = (pumpConText as NSString).floatValue
         
         let graphX = Float(graphDisplay.frame.origin.x)
-        let graphY = Float(graphDisplay.frame.origin.y)
+        let graphY = Float(graphDisplay.frame.origin.y) - 3
         let graphWidth = Float(graphDisplay.bounds.width)
         let graphHeight = Float(graphDisplay.bounds.height)
         
@@ -1151,14 +1222,14 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         
         //points are calculated and added to array
         xCoord += ((hour1 / 24.00) + (minute1 / (24.00 * 60.00))) * graphWidth
-        let maxYValue = multirateDoseSlider1.maximumValue
-        yCoord = (multirateDoseSlider1.value / maxYValue) * graphHeight
+        let maxYValue = multirateDoseSlider1.maximumValue / pumpConFloat
+        yCoord = ((multirateDoseSlider1.value / pumpConFloat)) * graphHeight
         var cSet: [Int] = [Int(xCoord), Int(yCoord)]
         coordArray.append(cSet)
         var date1 = multirateStartField1.text
         var date2 = multirateStartField1.text
-        var sliderVal = multirateDoseSlider1.value
-        var prevSliderVal = multirateDoseSlider2.value
+        var sliderVal = multirateDoseSlider1.value / pumpConFloat
+        var prevSliderVal = multirateDoseSlider2.value / pumpConFloat
         while(c < stepperState)
         {
             if(c == 0)
@@ -1184,16 +1255,15 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
             
             if(stepperState - c == 1)
             {
-                sliderVal = multirateDoseSlider1.value
-                if(stepperState == 2){ prevSliderVal = multirateDoseSlider2.value }
-                if(stepperState == 3){ prevSliderVal = multirateDoseSlider3.value }
-                if(stepperState == 4){ prevSliderVal = multirateDoseSlider4.value }
+                sliderVal = multirateDoseSlider1.value / pumpConFloat
+                if(stepperState == 2){ prevSliderVal = multirateDoseSlider2.value / pumpConFloat }
+                if(stepperState == 3){ prevSliderVal = multirateDoseSlider3.value / pumpConFloat }
+                if(stepperState == 4){ prevSliderVal = multirateDoseSlider4.value / pumpConFloat }
                 date2 = multirateStartField1.text
             }
-            else if(c == 0){ sliderVal = multirateDoseSlider2.value }
-            else if(c == 1){ sliderVal = multirateDoseSlider3.value }
-            else if(c == 2){ sliderVal = multirateDoseSlider4.value }
-            
+            else if(c == 0){ sliderVal = multirateDoseSlider2.value / pumpConFloat }
+            else if(c == 1){ sliderVal = multirateDoseSlider3.value / pumpConFloat }
+            else if(c == 2){ sliderVal = multirateDoseSlider4.value / pumpConFloat }
             xCoord += calculateXDistance(startTime: date1!, endTime: date2!, gWidth: graphWidth)
             if(xCoord == graphWidth)
             {
@@ -1238,7 +1308,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
                 cSet = [Int(xCoord), Int(yCoord)]
                 coordArray.append(cSet)
             }
-            yCoord = (sliderVal / maxYValue) * graphHeight
+            yCoord = (sliderVal) * graphHeight
             cSet = [Int(xCoord), Int(yCoord)]
             coordArray.append(cSet)
             c += 1
@@ -1268,7 +1338,7 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         shapeLayer.path = path.cgPath
         shapeLayer.lineCap = kCALineCapRound
         shapeLayer.lineJoin = kCALineJoinRound
-        view.layer.addSublayer(shapeLayer)
+        scrollView.layer.addSublayer(shapeLayer)
         
         //save shape layer to viewcontroller
         self.multiRateShapeLayer = shapeLayer
@@ -1331,9 +1401,15 @@ class View_CompareFlow: UIViewController, UITextFieldDelegate
         self.multirateControlStack3.alpha = 0.0
         self.multirateControlStack4.alpha = 0.0
         
-        genConstantGraph()
-        genPeriodicGraph()
-        genMultiRateGraph()
+        let pumpConText = masterConcentrationField.text!
+        let pumpConFloat = (pumpConText as NSString).floatValue
+        let dosePerClick = accumVol * pumpConFloat
+        let dosePerClickRounded = Double(dosePerClick * 100).rounded() / 100
+        dosePerClickField.text = "\(dosePerClickRounded)" + " " + unitLabel
+        
+        updateConstant()
+        updatePeriodic()
+        updateMultirate()
     }
     
     override func didReceiveMemoryWarning()
